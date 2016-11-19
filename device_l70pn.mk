89688f0ac674705bacc83c97d32d507db4061231
geekydoc <panospcx@gmail.com>
2016-11-19 10:40:36 +0200

    Update device_l70pn.mk

--- device_l70pn.mk
+++ device_l70pn.mk
@@ -19,39 +19,25 @@ $(call inherit-product, frameworks/native/build/phone-hdpi-512-dalvik-heap.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Specific overlay
-DEVICE_PACKAGE_OVERLAYS += device/lge/l70pn/overlay
+DEVICE_PACKAGE_OVERLAYS += device/huawei/Y625/overlay
 
 PRODUCT_PACKAGES += Torch
 
-# NFC packages
-PRODUCT_PACKAGES += \
-    NfcNci \
-    libnfc-nci \
-    nfc_nci.pn54x.default \
-    Tag \
-    com.android.nfc_extras
-
-NFCEE_ACCESS_PATH := device/lge/l70pn/prebuilt/etc/nfcee_access.xml
-
 PRODUCT_COPY_FILES += \
-    device/lge/l70pn/mixer_paths.xml:system/etc/mixer_paths.xml \
-    device/lge/l70pn/prebuilt/etc/thermal-engine-8610.conf:system/etc/thermal-engine-8610.conf \
+    device/huawei/Y625/mixer_paths.xml:system/etc/mixer_paths.xml \
+    device/huawei/Y625/prebuilt/etc/thermal-engine-8610.conf:system/etc/thermal-engine-8610.conf \
     frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
     frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml \
     frameworks/native/data/etc/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml \
-    device/lge/l70pn/prebuilt/etc/libnfc-brcm.conf:system/etc/libnfc-brcm.conf \
-    device/lge/l70pn/prebuilt/etc/nfc-nci.conf:system/etc/nfc-nci.conf \
-    device/lge/l70pn/prebuilt/etc/libnfc-nxp.conf:system/etc/libnfc-nxp.conf \
-    device/lge/l70pn/prebuilt/etc/nfcee_access.xml:system/etc/nfcee_access.xml \
-    device/lge/l70pn/fstab.msm8610:root/fstab.msm8610
+    device/huawei/Y625/fstab.msm8610:root/fstab.msm8610
 
 PRODUCT_LOCALES := en_US
 PRODUCT_LOCALES += hdpi
 PRODUCT_AAPT_CONFIG := normal hdpi
 PRODUCT_AAPT_PREF_CONFIG := hdpi
 
-$(call inherit-product, vendor/lge/l70pn/l70pn-vendor.mk)
+$(call inherit-product, vendor/huawei/Y625/Y625-vendor.mk)
 
 # Inherit from msm8610-common
-$(call inherit-product, device/lge/msm8610-common/msm8610.mk)
+$(call inherit-product, device/huawei/msm8610-common/msm8610.mk)
 
