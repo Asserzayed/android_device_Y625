5dd58156fb8460ca8ebcd8e89d82da384264ade4
geekydoc <panospcx@gmail.com>
2016-11-19 10:38:14 +0200

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -3,10 +3,10 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Boot animation
 TARGET_SCREEN_WIDTH := 480
-TARGET_SCREEN_HEIGHT := 800
+TARGET_SCREEN_HEIGHT := 854
 
 # Release name
-PRODUCT_RELEASE_NAME := LG L Fino
+PRODUCT_RELEASE_NAME := Y625
 PRODUCT_NAME := cm_l70pn
 
 $(call inherit-product, device/lge/l70pn/full_l70pn.mk)
